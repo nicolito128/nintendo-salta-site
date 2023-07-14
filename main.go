2b0c108ea8891c0a69323de90a2e278bfc73f992
nicolito128 <nicolito128@hotmail.com>
2023-07-13 22:59:56 -0300

    Fix static path

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 	// Definiendo el enrutador.
 	// Se encarga de manejar las rutas del sitio y renderizar respuestas.
 	router := fiber.New(fiber.Config{Views: engine})
-	router.Static("/assets", "./public/static")
+	router.Static("/assets", "./public/assets")
 
 	// Obteniendo el puerto de ejecución del servidor.
 	// Es posible ingresarlo por defecto desde consola.
